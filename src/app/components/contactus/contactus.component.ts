08e6b796a050fb713e2b2dc31ad0e3f06a36f50f
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-03-09 23:55:46 +0200

    added input fields and styled with material

--- src/app/components/contactus/contactus.component.ts
+++ src/app/components/contactus/contactus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactusComponent implements OnInit {
 
+  id!: number;
+  fullname!: String;
+  email!: string;
+  message!: string;
+
   constructor() { }
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
   ngOnInit(): void {
+  }
+  submitForm() {
+    alert('Submitted Successful');
   }
 
 }
