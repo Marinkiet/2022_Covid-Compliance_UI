d2bfe5738a1b89d7feb0776be4f0980d39c2d9f6
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-03-10 01:13:34 +0200

    changed background to backdrop

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { HealthFormComponent } from '../health-form/health-form.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
+import { delay } from 'rxjs/operators';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
@@ -9,7 +15,10 @@ export class UserProfileComponent implements OnInit {
   userProfile !: FormGroup;
   actionBtn:string = "Save";
 
-  constructor( private formBuilder: FormBuilder) { }
+  constructor( 
+    private formBuilder: FormBuilder,
+    private dialog: MatDialog,
+    private observer: BreakpointObserver) { }
 
   ngOnInit(): void {
     this.userProfile = this.formBuilder.group({
@@ -23,5 +32,27 @@ export class UserProfileComponent implements OnInit {
       newPassword: ['', Validators.required],
 
     });
+  }
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+
+  openDialog() {
+    this.dialog.open(HealthFormComponent, {
+      width: '30%'
+    });
+  }
+  ngAfterViewInit() {
+    this.observer
+      .observe(['(max-width: 800px)'])
+      .pipe(delay(1))
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      });
   }
 }
