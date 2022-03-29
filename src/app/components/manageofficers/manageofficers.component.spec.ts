836b0d53dcbfca0b2cc7e5c4737a97a4904aef25
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-03-30 01:43:23 +0200

    animated videos

---
+++ src/app/components/manageofficers/manageofficers.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ManageofficersComponent } from './manageofficers.component';
+
+describe('ManageofficersComponent', () => {
+  let component: ManageofficersComponent;
+  let fixture: ComponentFixture<ManageofficersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ManageofficersComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ManageofficersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
