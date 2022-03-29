5594ab633c81ffbe12cb4015ae308d40d7b67e1f
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-03-29 19:54:43 +0200

    removed health form button

--- src/app/components/contactus/contactus.component.ts
+++ src/app/components/contactus/contactus.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { HealthFormComponent } from '../health-form/health-form.component';
-import { MatDialog } from '@angular/material/dialog';
-import { ViewChild } from '@angular/core';
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { MatSidenav } from '@angular/material/sidenav';
-import { delay } from 'rxjs/operators';
+
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
@@ -23,33 +18,12 @@ export class ContactusComponent implements OnInit {
   }
   ngOnInit(): void {
   }
-  @ViewChild(MatSidenav)
-  sidenav!: MatSidenav;
+
 
   constructor(
-    private dialog: MatDialog,
-    private observer: BreakpointObserver) {}
+   ) {}
     
-    openDialog() {
-      this.dialog.open(HealthFormComponent, {
-        width: '30%'
-      });
-  
-    }
-  ngAfterViewInit() {
-    this.observer
-      .observe(['(max-width: 800px)'])
-      .pipe(delay(1))
-      .subscribe((res) => {
-        if (res.matches) {
-          this.sidenav.mode = 'over';
-          this.sidenav.close();
-        } else {
-          this.sidenav.mode = 'side';
-          this.sidenav.open();
-        }
-      });
-  }
+    
  
   
 }
