90a30a351265e3df5ec87241db6d0ebf197fbbcb
SihleMagagula <93584282+SihleMagagula@users.noreply.github.com>
2022-05-04 17:56:36 +0200

    Add files via upload

--- contactus.component.ts
+++ contactus.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.component.html',
   styleUrls: ['./contactus.component.css']
-  
 })
 export class ContactusComponent implements OnInit {
 
-  name!: string;
-  lastname!: String;
+  id!: number;
+  fullname!: String;
   email!: string;
   message!: string;
-
-  constructor() { }
-
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  submitForm() {
+    alert('Submitted Successful');
+  }
   ngOnInit(): void {
   }
-  submitForm() {
-    alert('Sibmitted Successful');
-  }
 
+
+  constructor(
+   ) {}
+    
+    
+ 
+  
 }
